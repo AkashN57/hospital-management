efc290e26d3f3c5a83c37ab3630c76c4d59fc88e
Madurapperuma A Anujaya <148967838+anujay00@users.noreply.github.com>
2024-12-27 12:23:00 +0530

    Homepage
    Sentences and words in home page.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -15,11 +15,12 @@ const Footer = () => {
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         <div>
           <img className="mb-5 w-40" src={assets.logo} alt="" />
-          <p className="w-full md:w-2/3 text-gray-600 leading-6">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+          <p className="w-full md:w-2/3 text-gray-600 leading-6 text-justify">
+            MediHouse Hospital offers exceptional healthcare services with a
+            team of trusted doctors, ensuring quality treatment. Patients can
+            conveniently book appointments online, saving time and effort.
+            Experience hassle-free scheduling and access to expert medical care,
+            all at your fingertips.
           </p>
         </div>
 
@@ -39,7 +40,7 @@ const Footer = () => {
         </div>
 
         <div>
-          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <p className="text-xl font-medium mb-5">Inquires</p>
           <ul className="flex flex-col gap-2 text-gray-600">
             <li>+9476767676</li>
             <li>group18@gmail.com</li>
