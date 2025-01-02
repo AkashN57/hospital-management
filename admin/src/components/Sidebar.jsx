89accc9494f762a8f95d3d38cd7bff90e72ef2b9
Madurapperuma A Anujaya <148967838+anujay00@users.noreply.github.com>
2025-01-02 20:58:28 +0530

    frontend abjusments

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -38,7 +38,7 @@ const Sidebar = () => {
           <p className='hidden md:block'>Staff List</p>
         </NavLink>
         <NavLink to={'/complaints'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
-          <img className='min-w-5' src={assets.people_icon} alt='' />
+          <img className='min-w-5' src={assets.complain_icon} alt='' />
           <p className='hidden md:block'>Complaints</p>
         </NavLink>
       </ul>}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         'auto':'repeat(auto-fill, minmax(200px, 1fr))'
       },
       colors:{
-        'primary':'#89e884'
+        'primary':'#4CBB17'
       }
     },
   },
